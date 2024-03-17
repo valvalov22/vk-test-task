6acc4ad05f382021f203644354cd812bdb5c9413
valvalov22 <vlrik@mail.ru>
2024-03-17 16:33:36 +0400

    view and panel func

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Panel, View } from "@vkontakte/vkui";
+import { CellButton, Group, Panel, View } from "@vkontakte/vkui";
 import { CatFact } from "../features/catFact";
 import { AgifyForm } from "../features/agify";
 import "@vkontakte/vkui/dist/vkui.css";
@@ -8,16 +8,27 @@ import "@vkontakte/vkui/dist/vkui.css";
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
+    const [activePanel, setActivePanel] = useState("main");
+
     return (
         <QueryClientProvider client={queryClient}>
-            <View nav="main" activePanel="main">
+            <View nav="main" activePanel={activePanel}>
                 <Panel id="main">
-                    <CatFact />
+                    <Group>
+                        <CatFact />
+                        <CellButton onClick={() => setActivePanel("second")}>
+                            Перейти, чтобы узнать свой возраст
+                        </CellButton>
+                    </Group>
                 </Panel>
-            </View>
-            <View nav="second" activePanel="second">
+
                 <Panel id="second">
-                    <AgifyForm />
+                    <Group>
+                        <AgifyForm />
+                        <CellButton onClick={() => setActivePanel("main")}>
+                            Перейти, чтобы получить рандомный факт
+                        </CellButton>
+                    </Group>
                 </Panel>
             </View>
         </QueryClientProvider>
